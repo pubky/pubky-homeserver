cb7f0624ac6a1d441afa58c2b755b359e279cc72
tomos <tomos@synonym.to>
2026-07-16 16:15:36 +0100

    fix: set default_test_db() connection string to DEFAULT_TEST_CONNECTION_STRING + pubky-test flag

--- pubky-homeserver/src/persistence/sql/connection_string.rs
+++ pubky-homeserver/src/persistence/sql/connection_string.rs
@@ -42,11 +42,16 @@ impl ConnectionString {
 
 #[cfg(any(test, feature = "testing"))]
 impl ConnectionString {
-    /// Returns a connection string for a test database.
-    /// This is a postgres database that is not real.
-    /// It is used as an indicator for a empheral test database.
+    /// Default connection string for ephemeral test databases.
+    ///
+    /// This is the same as [`super::sql_db::DEFAULT_TEST_CONNECTION_STRING`] with
+    /// `?pubky-test=true` appended to signal ephemeral database creation.
     pub fn default_test_db() -> Self {
-        Self::new("postgres://postgres:postgres@localhost:5432/postgres?pubky-test=true").unwrap()
+        Self::new(&format!(
+            "{}?pubky-test=true",
+            super::sql_db::DEFAULT_TEST_CONNECTION_STRING
+        ))
+        .unwrap()
     }
 
     /// Returns true if the connection string is for a test database.
